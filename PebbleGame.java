6433e56f92551e4a70a6b565a679b7dd1f2e3f2a
cameronstackhouse <48853306+cameronstackhouse@users.noreply.github.com>
2021-10-13 14:27:50 +0100

    Add files via upload

---
+++ PebbleGame.java
@@ -0,0 +1,12 @@
+public class PebbleGame {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Pebble Game!");
+        System.out.println("You will be asked to enter the number of players \nand then the location of the three files" +
+                " in turn containing comma separated integer values for the pebble weights. \nThe integer values must be" +
+                " strictly positive. The game will then be simulated, and output written to files in this directory.");
+
+    }
+}
