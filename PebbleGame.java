b53247b484e5275428c44eed762268a6ce56b2d1
cameronstackhouse <stackhousecameron@gmail.com>
2021-10-14 16:36:42 +0100

    Test

--- PebbleGame.java
+++ PebbleGame.java
@@ -12,19 +12,25 @@ public class PebbleGame {
         public void run() {}
     }
 
-    public static ArrayList<Pebble> createBag(String filename, int bagIndex) throws FileNotFoundException, NumberFormatException {
+    public static ArrayList<Pebble> readBag(String filename, int bagIndex) throws NumberFormatException {
         ArrayList<Pebble> pebbles = new ArrayList<>();
-        Scanner scanner = new Scanner(new File(filename));
 
-        scanner.useDelimiter(",");
+        try {
+            Scanner scanner = new Scanner(new File(filename));
+            scanner.useDelimiter(",");
 
-        while (scanner.hasNext()){
+            while (scanner.hasNext()){
 
-            int weight = Integer.parseInt(scanner.next().strip());
+                int weight = Integer.parseInt(scanner.next().strip());
 
-            Pebble pebble = new Pebble(weight,bagIndex);
+                Pebble pebble = new Pebble(weight,bagIndex);
 
-            pebbles.add(pebble);
+                pebbles.add(pebble);
+
+            }
+        } catch (FileNotFoundException e){
+            PebbleGame.csvNotFoundError();
+        } catch (NullPointerException e){
 
         }
 
@@ -38,48 +44,72 @@ public class PebbleGame {
                 " in turn containing comma separated integer values for the pebble weights. \nThe integer values must be" +
                 " strictly positive. The game will then be simulated, and output written to files in this directory.");
 
-        boolean valid = false;
-        while (!valid) { //Keeps repeating until the user provides valid input for both number of players and filepaths
-            Scanner reader = new Scanner(System.in);
-            String bag0FilePath = null, bag1FilePath = null, bag2FilePath = null;
-            Bag X = null;
-            Bag Y = null;
-            Bag Z = null;
+        Scanner reader = new Scanner(System.in);
+
+        String bag0FilePath = null;
+        String bag1FilePath = null;
+        String bag2FilePath = null;
+
+
+        Bag X = null;
+        Bag Y = null;
+        Bag Z = null;
+
+
+        //Gets the user input for the number of players and parses it as an int
+        while (true){
             try {
-
-                //Gets the user input for the number of players and parses it as an int
-                System.out.println("Enter the number of players:");
+                System.out.println("Enter number of players:");
                 int numPlayers = reader.nextInt();
+                break;
+            } catch (InputMismatchException e){
+                PebbleGame.playerNumError();
+                reader.next();
+            }
+        }
 
-                //Gets the file path names for the
+        while (true) {
+            try {
                 System.out.println("Enter the location of bag number 0 to load:");
                 bag0FilePath = reader.next();
-
-                System.out.println("Enter the location of bag number 1 to load:");
-                bag1FilePath = reader.next();
-
-                System.out.println("Enter the location of bag number 2 to load:");
-                bag2FilePath = reader.next();
-
-                valid = true;
-
-            } catch (InputMismatchException e) {
-                PebbleGame.playerNumError();
-            }
-
-            try{
-                X = new Bag(createBag(bag0FilePath, 0));
-                Y = new Bag(createBag(bag1FilePath, 1));
-                Z = new Bag(createBag(bag2FilePath, 2));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+                X = new Bag(readBag(bag0FilePath, 0));
+                break;
             } catch (NumberFormatException e){
                 PebbleGame.csvFormatError();
             }
-
-            System.out.println(X.valueOfPebbles());
         }
 
+        while (true) {
+
+            System.out.println("Enter the location of bag number 1 to load:");
+            bag1FilePath = reader.next();
+            Y = new Bag(readBag(bag1FilePath, 1));
+        }
+
+        while (true) {
+            System.out.println("Enter the location of bag number 2 to load:");
+            bag2FilePath = reader.next();
+            Z = new Bag(readBag(bag2FilePath, 2));
+        }
+
+    }
+
+    public static Bag createBag(int bagIndex){
+        Scanner reader = new Scanner(System.in);
+        Bag newBag;
+        while (true){
+            try {
+                System.out.println("Enter the location of bad number " + bagIndex + " to load:");
+                String fPath = reader.next();
+                newBag = new Bag(readBag(fPath, bagIndex));
+                break;
+
+            } catch (NumberFormatException e){
+                PebbleGame.csvFormatError();
+            }
+        }
+        reader.close();
+        return newBag;
     }
 
     private static void playerNumError(){
@@ -92,7 +122,7 @@ public class PebbleGame {
     }
 
     private static void csvNotFoundError(){
-
+        System.out.println("No such file exists.");
     }
 
 }
