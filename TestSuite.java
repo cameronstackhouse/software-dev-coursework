d070cc410459efb61d1484e9fcd4cf5aa9d3c420
cameronstackhouse <48853306+cameronstackhouse@users.noreply.github.com>
2021-11-02 14:55:21 +0000

    Delete TestSuite.java

--- TestSuite.java
+++
@@ -1,6 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({TestBag.class, TestPebble.class, TestPlayer.class, TestPebbleGame.class})
-public class TestSuite {}
